2b1311042fc0fde63fc8f4c95fd86c257f3f3bf8
gopikannan.p <srigopikannan@gmail.com>
2017-07-06 20:00:29 +0530

    get data from JSON

--- www/modules/project/controllers/domain_list_ctrl.js
+++ www/modules/project/controllers/domain_list_ctrl.js
@@ -1,9 +1,15 @@
 angular.module('tripleN')
-.controller('domain_list_ctrl',['$scope', '$state',function($scope, $state){
-
-$scope.domainList = ["Embeded", "DSP/DIP", "Java", "Dot Net", "NS2", "Android","VLSI", "PLC/SCADA", "Power Electronics", "Power Systems"]
+.controller('domain_list_ctrl',['$scope', '$state','domain_factory', '$rootScope', function($scope, $state, domain_factory, $rootScope){
+$scope.domainListtest = [];
+//$scope.domainList = ["Embeded", "DSP/DIP", "Java", "Dot Net", "NS2", "Android","VLSI", "PLC/SCADA", "Power Electronics", "Power Systems"];
+domain_factory.getDomainList();
  $scope.getMsg = function(){
+	console.log("Initial :"+$rootScope.domainListData)
  	alert('welcome to projects');
+ 	for(var i = 0; i< $rootScope.domainListData.data.length; i++){
+ 		$scope.domainListtest = $rootScope.domainListData.data[i];
+ 		console.log("Title : "+$rootScope.domainListData.data[i])
+ 	}
  }
 
 }])
